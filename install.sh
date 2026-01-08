7cc5d72fcfeaf2c1fe6bc8f62c7d4e88759b3527
Lucas Moidel <lucas122133@gmail.com>
2026-01-08 13:28:18 -0800

    script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp * ~/
+cd ~/
+git clone https://github.com/ohmyzsh/ohmyzsh.git
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo chsh -s "$zsh" "$USER"
+
+
+rm -rf dotfiles
+rm install.sh
