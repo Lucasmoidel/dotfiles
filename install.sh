c8c5f5518f370aeea6a3d8dfde82fe641695da13
Lucas Moidel <lucas122133@gmail.com>
2026-01-09 09:35:58 -0800

    copy dot files

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cp * ~/
+cp * ~/ -r
+cp .* ~/ -r
 cd ~/
 git clone https://github.com/ohmyzsh/ohmyzsh.git
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
